b0428683608288e6f236b8ce5d22440aa1dd3d95
Sebastian Mekal <sebitg@gmail.com>
2014-12-14 01:47:05 +0100

    Step3 - sending mails with status

--- src/main/groovy/pl/sebitg/tutorials/gradleplugin/TestAndPmdResultsSenderPlugin.groovy
+++ src/main/groovy/pl/sebitg/tutorials/gradleplugin/TestAndPmdResultsSenderPlugin.groovy
@@ -3,14 +3,22 @@ package pl.sebitg.tutorials.gradleplugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import pl.sebitg.tutorials.gradleplugin.tasks.SendEmailTask
+
 /**
  * Plugin extension class
  * @author s.mekal
  *
  */
 class PluginExtensionClass {
-	String someVar1 = "default val";
-	String someVar2 = "default-val2";
+	String from = "Sebastian Mekal <sebitg@gmail.com>"
+	String username = "sebitg@gmail.com"
+	String password = "password"
+	String host = "smtp.gmail.com"
+	Boolean auth = true
+	Boolean sslEnable = true
+	Integer port = 465
+	String mails = "btcgrouppl@gmail.com"
 }
 
 /**
@@ -24,15 +32,24 @@ class TestAndPmdResultsSenderPlugin implements Plugin<Project> {
 		
 		arg0.extensions.create("tutorialArgs", PluginExtensionClass)
 		
+		//Old task remain the same
 		arg0.task('someTask') << {
 			def someSysProperty = System.properties['someArg']
 			println "Hello someTask world!"
 			println "System arg: " + someSysProperty
 			println "-----------------------------"
 			println "Printing tutorialArgs configuration :"
-			println "${project.tutorialArgs.someVar1}"
-			println "${project.tutorialArgs.someVar2}"
+			println "${project.tutorialArgs.from}"
+			println "${project.tutorialArgs.username}"
 		}
 		
+		//Adding new sendemail task, and execution rules
+		arg0.task("sendemail", type: SendEmailTask)
+		arg0.tasks.getByName("test")
+				.finalizedBy("sendemail")
+		arg0.tasks.getByName("compileJava")
+				.finalizedBy("sendemail")
+		arg0.tasks.getByName("build")
+				.finalizedBy("sendemail")
 	}
 }
